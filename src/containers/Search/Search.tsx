d0bc28fecbca517d16f014c437a4a93a7424e7ae
huan2401 <huannv.bhsoft@gmail.com>
2022-08-18 15:58:41 +0700

    add event onEnter

--- src/containers/Search/Search.tsx
+++ src/containers/Search/Search.tsx
@@ -1,16 +1,20 @@
 import React, { useState } from 'react';
 import { Button, Input, AutoComplete } from 'antd';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { SearchWrapper } from './CustomStyled';
 
 const Search: React.FC = () => {
+  const history = useHistory();
   const handleSearch = () => {};
+  const handleEnter = () => {
+    history.push('/search-result');
+  };
   return (
     <SearchWrapper>
       <div className="search-content">
         <div className="search-content-head">
           <h1>Search</h1>
-          <Input placeholder="Search" onChange={handleSearch} />
+          <Input placeholder="Search" onChange={handleSearch} onPressEnter={handleEnter}/>
         </div>
         <div>
           <Link to="/salary">業界</Link>
