498f7e16fa07be327b4bee5bcf8a8d99ceb649d5
AVEPOINT\liam.nguyen <Liam.nguyen@avepoint.com>
2022-11-25 00:21:48 +0700

    fix sort

--- src/containers/SearchResult/SearchResult.tsx
+++ src/containers/SearchResult/SearchResult.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 import { useHistory } from 'react-router-dom';
 import { SearchResultWrapper } from './CustomStyled';
 
-const fakeDataItem = [
+const fakeDataItem1 = [
   {
     name: '株式会社APパートナーズ',
     location: '新宿区新宿2-6-4 新宿通り東洋ビル10F 東京',
@@ -24,20 +24,51 @@ const fakeDataItem = [
       'https://img.meta.com.vn/Data/image/2022/01/13/anh-dep-thien-nhien-3.jpg',
   },
 ];
+const fakeDataItem2 = [
+  {
+    name: '株式会社ITソリューションズ',
+    location: '新宿区守口市佐太東町3-101-5',
+    image:
+      'https://img.thuthuattinhoc.vn/uploads/2019/01/13/anh-dep-vung-nui-viet-nam_105620901.jpeg',
+  },
+  {
+    name: '新宿区名古屋市北区金田町4-721-1',
+    location: '勤 務 地一都三県（東京都・埼玉県・千葉県・神奈川 県）',
+    image:
+      'https://img.meta.com.vn/Data/image/2022/01/13/anh-dep-thien-nhien-3.jpg',
+  },
+  {
+    name: '株式会社APパートナーズ',
+    location: '新宿区新宿2-6-4 新宿通り東洋ビル10F 東京',
+    image:
+      'https://s3.ap-northeast-1.amazonaws.com/uploaded-files.my-sto.com/companies/production/827680d7b35220e49e39f6899fddaecf.png',
+  },
+];
+const fakeDataItem3 = [
+  {
+    name: '新宿区名古屋市北区金田町4-721-1',
+    location: '勤 務 地一都三県（東京都・埼玉県・千葉県・神奈川 県）',
+    image:
+      'https://img.meta.com.vn/Data/image/2022/01/13/anh-dep-thien-nhien-3.jpg',
+  },
+  {
+    name: '株式会社APパートナーズ',
+    location: '新宿区新宿2-6-4 新宿通り東洋ビル10F 東京',
+    image:
+      'https://s3.ap-northeast-1.amazonaws.com/uploaded-files.my-sto.com/companies/production/827680d7b35220e49e39f6899fddaecf.png',
+  },
+  {
+    name: '株式会社ITソリューションズ',
+    location: '新宿区守口市佐太東町3-101-5',
+    image:
+      'https://img.thuthuattinhoc.vn/uploads/2019/01/13/anh-dep-vung-nui-viet-nam_105620901.jpeg',
+  },
+];
 
 const SearchResult: React.FC = () => {
-  const [data, setData] = React.useState(fakeDataItem);
+  const [data, setData] = React.useState(fakeDataItem1);
   const history = useHistory();
 
-  const handleSort = () => {
-    setData(prev => {
-      let temp = [...prev].sort(() => 0.5 - Math.random());
-      while (temp[0].name === prev[0].name) {
-        temp = [...prev].sort(() => 0.5 - Math.random());
-      }
-      return temp;
-    });
-  };
   const onFinish = (values: any) => {
     console.log('Success:', values);
   };
@@ -73,9 +104,9 @@ const SearchResult: React.FC = () => {
           </div>
         </div>
         <div className="searchResult-content-filter">
-          <p onClick={handleSort}>新着順</p>
-          <p onClick={handleSort}>締切間近順</p>
-          <p onClick={handleSort}>おすすめ順</p>
+          <p onClick={() => setData(fakeDataItem1)}>新着順</p>
+          <p onClick={() => setData(fakeDataItem2)}>締切間近順</p>
+          <p onClick={() => setData(fakeDataItem3)}>おすすめ順</p>
         </div>
         <div className="searchResult-content-list-item">
           {data.map((item, index) => {
