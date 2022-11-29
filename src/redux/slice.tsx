3853791336457ff4cd4928e6c00663a7da0e2251
minhnt <minhnt1@coccoc.com>
2022-11-29 14:22:42 +0700

    add code prototype

--- src/redux/slice.tsx
+++ src/redux/slice.tsx
@@ -1,12 +1,6 @@
-import React, { useState } from 'react';
-import { Button, Input, AutoComplete, Form } from 'antd';
-import { Link, useHistory } from 'react-router-dom';
-import { SearchResultWrapper } from './CustomStyled';
-<<<<<<< Updated upstream
-import { SearchOutlined } from '@ant-design/icons';
-=======
+import { createSlice } from '@reduxjs/toolkit';
 
-const fakeDataItem1 = [
+const fakeData = [
   {
     id: 0,
     name: '株式会社APパートナーズ',
@@ -43,8 +37,7 @@ const fakeDataItem1 = [
     image:
       'https://img.meta.com.vn/Data/image/2022/01/13/anh-dep-thien-nhien-3.jpg',
   },
-];
-const fakeDataItem2 = [
+
   {
     id: 3,
     name: '株式会社ITソリューションズ',
@@ -81,8 +74,7 @@ const fakeDataItem2 = [
     image:
       'https://s3.ap-northeast-1.amazonaws.com/uploaded-files.my-sto.com/companies/production/827680d7b35220e49e39f6899fddaecf.png',
   },
-];
-const fakeDataItem3 = [
+
   {
     id: 6,
     name: '株式会社エンジニアリング',
@@ -120,96 +112,91 @@ const fakeDataItem3 = [
       'https://img.thuthuattinhoc.vn/uploads/2019/01/13/anh-dep-vung-nui-viet-nam_105620901.jpeg',
   },
 ];
->>>>>>> Stashed changes
 
-const SearchResult: React.FC = () => {
-  const history = useHistory();
-  const onFinish = (values: any) => {
-    console.log('Success:', values);
-  };
+// const fakeData = [
+//   {
+//     id: 0,
+//     name: '株式会社APパートナーズ',
+//     location: '新宿区新宿2-6-4 新宿通り東洋ビル10F 東京',
+//     image:
+//       'https://s3.ap-northeast-1.amazonaws.com/uploaded-files.my-sto.com/companies/production/827680d7b35220e49e39f6899fddaecf.png',
+//   },
+//   {
+//     id: 1,
+//     name: '株式会社ITソリューションズ',
+//     location: '新宿区守口市佐太東町3-101-5',
+//     image:
+//       'https://img.thuthuattinhoc.vn/uploads/2019/01/13/anh-dep-vung-nui-viet-nam_105620901.jpeg',
+//   },
+//   {
+//     id: 2,
+//     name: '新宿区名古屋市北区金田町4-721-1',
+//     location: '勤 務 地一都三県（東京都・埼玉県・千葉県・神奈川 県）',
+//     image:
+//       'https://img.meta.com.vn/Data/image/2022/01/13/anh-dep-thien-nhien-3.jpg',
+//   },
+//   {
+//     id: 3,
+//     name: '株式会社ITソリューションズ',
+//     location: '新宿区守口市佐太東町3-101-5',
+//     image:
+//       'https://img.thuthuattinhoc.vn/uploads/2019/01/13/anh-dep-vung-nui-viet-nam_105620901.jpeg',
+//   },
+//   {
+//     id: 4,
+//     name: '新宿区名古屋市北区金田町4-721-1',
+//     location: '勤 務 地一都三県（東京都・埼玉県・千葉県・神奈川 県）',
+//     image:
+//       'https://img.meta.com.vn/Data/image/2022/01/13/anh-dep-thien-nhien-3.jpg',
+//   },
+//   {
+//     id: 5,
+//     name: '株式会社APパートナーズ',
+//     location: '新宿区新宿2-6-4 新宿通り東洋ビル10F 東京',
+//     image:
+//       'https://s3.ap-northeast-1.amazonaws.com/uploaded-files.my-sto.com/companies/production/827680d7b35220e49e39f6899fddaecf.png',
+//   },
+//   {
+//     id: 6,
+//     name: '新宿区名古屋市北区金田町4-721-1',
+//     location: '勤 務 地一都三県（東京都・埼玉県・千葉県・神奈川 県）',
+//     image:
+//       'https://img.meta.com.vn/Data/image/2022/01/13/anh-dep-thien-nhien-3.jpg',
+//   },
+//   {
+//     id: 7,
+//     name: '株式会社APパートナーズ',
+//     location: '新宿区新宿2-6-4 新宿通り東洋ビル10F 東京',
+//     image:
+//       'https://s3.ap-northeast-1.amazonaws.com/uploaded-files.my-sto.com/companies/production/827680d7b35220e49e39f6899fddaecf.png',
+//   },
+//   {
+//     id: 8,
+//     name: '株式会社ITソリューションズ',
+//     location: '新宿区守口市佐太東町3-101-5',
+//     image:
+//       'https://img.thuthuattinhoc.vn/uploads/2019/01/13/anh-dep-vung-nui-viet-nam_105620901.jpeg',
+//   },
+// ];
 
-  const onFinishFailed = (errorInfo: any) => {
-    console.log('Failed:', errorInfo);
-  };
+const initialState: any[] = [];
 
-  return (
-    <SearchResultWrapper>
-      <div className="searchResult-content">
-        <div className="searchResult-content-head">
-          <h1>Search</h1>
-          {/* <Input placeholder="Search" onChange={() => {}} /> */}
-          <Form
-            name="form-search"
-            layout="inline"
-            onFinish={onFinish}
-            onFinishFailed={onFinishFailed}
-            autoComplete="off"
-          >
-            <Form.Item name="search">
-              <Input placeholder="Search" onChange={() => {}} />
-            </Form.Item>
-            <Form.Item>
-              <Button type="primary" htmlType="submit">
-                <SearchOutlined />
-              </Button>
-            </Form.Item>
-          </Form>
-          <div className="searchResult-amount">
-<<<<<<< Updated upstream
-            <p>123 result</p>
-=======
-            <p>検索結果3件</p>
->>>>>>> Stashed changes
-          </div>
-        </div>
-        <div>
-          <p>新着順</p>
-          <p>締切間近順</p>
-          <p>おすすめ順</p>
-        </div>
-<<<<<<< Updated upstream
-        <div
-          className="searchResult-item"
-          onClick={() => history.push('/corporateinfo')}
-        >
-          <div>
-            <img
-              src="https://s3.ap-northeast-1.amazonaws.com/uploaded-files.my-sto.com/companies/production/827680d7b35220e49e39f6899fddaecf.png"
-              alt="#"
-            />
-          </div>
-          <div>
-            <p>業種</p>
-            <p>勤 務 地一都三県（東京都・埼玉県・千葉県・神奈川 県）</p>
-          </div>
-=======
-        <div className="searchResult-content-list-item">
-          {data.map((item, index) => {
-            return (
-              <div
-                key={index}
-                className="searchResult-item"
-                onClick={() => {
-                  history.push('/corporateinfo');
-                  dispatch(getItem(item.id));
-                }}
-              >
-                <div>
-                  <img src={item.image} alt="#" />
-                </div>
-                <div>
-                  <p>{item.name}</p>
-                  <p>勤務地</p>
-                  <p>{item.dutyStation}</p>
-                </div>
-              </div>
-            );
-          })}
->>>>>>> Stashed changes
-        </div>
-      </div>
-    </SearchResultWrapper>
-  );
-};
+const appSlice = createSlice({
+  name: 'app',
+  initialState,
+  reducers: {
+    getItem(state, action) {
+      console.log('redux', state);
+      console.log('redux action', action);
 
-export default SearchResult;
+      return [fakeData.find(item => item.id === action.payload)];
+    },
+    getCurrentItem(state) {
+      return state;
+    },
+  },
+});
+
+const { reducer } = appSlice;
+export const { getItem, getCurrentItem } = appSlice.actions;
+export default reducer;
